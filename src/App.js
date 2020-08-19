b23313f2a117b8cb09c020a30f4364f8fa485ded
sperkn <nicholas.kreps@gmail.com>
2020-08-18 22:47:09 -0400

    added form component, hooks to handle state, and custom hook for reusable form inputs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Rocko" animal="Dog" breed="Havanese" />
-      <Pet name="Luna" animal="Bird" breed="Cockatiel" />
-      <Pet name="Roxy" animal="Cat" breed="Mixed" />
+      <SearchParams />
     </div>
   );
 };
