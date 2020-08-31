645fdf7731ff1be7db2b884901c6f7470b4d3bea
sperkn <nicholas.kreps@gmail.com>
2020-08-30 22:56:10 -0400

    Context example for button color theme

--- src/Details.js
+++ src/Details.js
@@ -2,16 +2,11 @@ import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
 
 class Details extends React.Component {
   state = { loading: true };
-  // constructor(props) {
-  //   super(props);
 
-  //   this.state = {
-  //     loading: true,
-  //   };
-  // }
   componentDidMount() {
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
@@ -38,7 +33,13 @@ class Details extends React.Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {(themeHook) => (
+              <button style={{ backgroundColor: themeHook[0] }}>
+                Adopt {name}
+              </button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
       </div>
